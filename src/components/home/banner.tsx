7d91d3ccc7c1c2d9149dc7102bb53090e4df64a5
bSchutters <bryan021@hotmail.be>
2025-09-13 16:30:10 +0200

    update team names + pics home

--- src/components/home/banner.tsx
+++ src/components/home/banner.tsx
@@ -6,7 +6,7 @@ export default function Banner() {
       <div className="absolute w-full h-full bg-spanish-bg/75 z-10" />
       <div className="absolute w-full h-full mask-b-from-100%">
         <Image
-          src="/assets/images/webp/spainPic3.webp"
+          src="/assets/images/webp/imageHome.webp"
           alt="Spain Picture"
           fill
           className="object-cover object-top"
@@ -14,8 +14,8 @@ export default function Banner() {
       </div>
       <div className="flex flex-col items-center justify-center text-center lg:text-4xl text-2xl lg:gap-4 z-20">
         <p>
-          <span className="font-bold text-spanish-accent">Amitié </span> et
-          <span className="font-bold text-spanish-accent"> passion</span>
+          <span className="font-bold text-spanish-accent-2">Amitié </span> et
+          <span className="font-bold text-spanish-accent-2"> passion</span>
         </p>
         <p>générations après générations</p>
       </div>
